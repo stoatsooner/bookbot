27e2511e39971149b5240b640032797a92516bcb
Monsooner <tcnchardelli@gmail.com>
2025-02-01 08:43:55 -0300

    finalized return message and descending order alphabetical count

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     word_count = 0
     character_count = {}
+    char_list = []
+
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         words = file_contents.split()
@@ -12,10 +14,25 @@ def main():
                 character_count[character] += 1
             else:
                 character_count[character] = 1
+    
+    for key, value in character_count.items():
+        if key.isalpha():
+            char_list.append({"character": key, "count": value})
 
+    def sort_on(item):
+        return item["count"]
 
+    char_list.sort(reverse=True, key=sort_on)        
 
-        print (f"This book has {word_count} words. Here's a counter for every character: {character_count}")
+    print (f"--- Begin report of books/frankenstein.txt ---")
+    print (f"This document has {word_count} words.")
+    
+    for item in char_list:
+        char = item["character"]
+        count = item["count"]
+        print (f"The '{char}' character was found {count} times.")
 
+    print("--- End report ---")
+    
 if __name__ == "__main__":
     main()
